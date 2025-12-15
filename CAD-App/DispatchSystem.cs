b5edec1f8251706ebcedf252d0231b390e29f4df
MrMister419 <andrei.swindon@gmail.com>
2025-12-15 18:10:47 +0000

    Initial commit, CAD and Server, OOP Event

---
+++ CAD-App/DispatchSystem.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EMS_NEA;
+
+class DispatchSystem
+{
+    private static List<Event> sentEvents;
+    private static readonly Random Random = new Random();
+    private static readonly HttpClient client = new HttpClient();
+    
+    public static async Task Main(string[] args)
+    {
+        sentEvents = DeserializeEvents();
+        Event randomEvent = GetRandomEvent(sentEvents);
+        Console.WriteLine(randomEvent);
+        Console.WriteLine(randomEvent.Serialize());
+        //randomEvent.StartEvent(sentEvents.Count);
+        //await SendEvent(randomEvent);
+        
+    }
+    
+    // Listen for client requests for events
+    private static void ListenToClient()
+    {
+        
+    }
+
+    private static List<Event> DeserializeEvents()
+    {
+        using (StreamReader reader = new StreamReader("SampleEvents.json"))
+        {
+            string json = reader.ReadToEnd();
+            Console.WriteLine(json);
+
+            List<Event> events = JsonSerializer.Deserialize<List<Event>>(json);
+            Console.WriteLine(events);
+
+            return events;
+        }
+    }
+
+    private static Event GetRandomEvent(List<Event> events)
+    {
+        int index = Random.Next(0, events.Count - 1);
+        return events[index];
+    }
+
+    private static async Task SendEvent(Event eventToSend)
+    {
+        string json = eventToSend.Serialize();
+        Console.WriteLine(json);
+        
+        try
+        {
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("http://localhost:50000", content);
+            
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine("POST request successful.");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine(e);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        sentEvents.Add(eventToSend);
+    }
+}
+
+class Event
+{
+    public int eventID { get; set; }
+    private string startTimestamp { get; set; }
+    private string resolvedTimestamp { get; set; }
+
+    public void StartEvent(int eventID)
+    {
+        this.eventID = eventID;
+        startTimestamp = DateTime.Now.ToLongTimeString();
+        Console.WriteLine("Event " + this.eventID + " started at " + startTimestamp);
+    }
+    
+    public void ResolveEvent()
+    {
+        resolvedTimestamp = DateTime.Now.ToLongTimeString();
+        Console.WriteLine("Event " + eventID + " resolved at " + resolvedTimestamp);
+    }
+    
+    public string Serialize()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EMS_NEA;
+
+class Server
+{
+    private static readonly HttpListener listener = new HttpListener();
+    private static Event receivedEvent;
+
+    public static async Task Main(string[] args)
+    {
+        Event _event = await ListenForEvents();
+        Console.WriteLine(SerializeEvent(receivedEvent));
+    }
+    
+
+    private static async Task<Event> ListenForEvents()
+    {
+        listener.Prefixes.Add("http://localhost:50000/");
+        listener.Start();
+        while (true)
+        {
+            // Wait for a POST request
+            HttpListenerContext context = await listener.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+            
+            // Get Event from POST request
+            if (request.HttpMethod == "POST")
+            {
+                using (StreamReader reader = new StreamReader(request.InputStream))
+                {
+                    string json = await reader.ReadToEndAsync();
+                    receivedEvent = DeserializeEvent(json);
+                }
+            }
+            
+            // Respond to POST request
+            HttpListenerResponse response = context.Response;
+            response.StatusCode = (int)HttpStatusCode.OK;
+            response.Close();
+            Console.WriteLine("POST request answered.");
+            return receivedEvent;
+        }
+    }
+    
+    private static Event DeserializeEvent(string jsonEvent)
+    {
+        Event serializedEvent = JsonSerializer.Deserialize<Event>(jsonEvent);
+
+        return serializedEvent;
+    }
+
+    private static string SerializeEvent(Event eventToSerialize)
+    {
+        string json = JsonSerializer.Serialize(eventToSerialize);
+        return json;
+    }
+}
+
+class Event
+{
+    private int eventID;
+    private string startTimestamp;
+    private string resolvedTimestamp;
+    
+}
