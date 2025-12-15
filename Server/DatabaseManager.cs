33e2bca046ce55e86ae828e4b17b7b366c53cbba
MrMister419 <andrei.swindon@gmail.com>
2025-12-15 23:46:08 +0000

    CAD, Server, DatabaseManager, transmission, database insertion, passkey in ENV variable

---
+++ Server/DatabaseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace EMS_NEA;
+
+internal class DatabaseManager
+{
+    private static OleDbConnection connection;
+
+    public DatabaseManager()
+    {
+        string passkey = System.Environment.GetEnvironmentVariable("DBPasskey");
+        string connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=EMS_NEA_Database.accdb;Jet OLEDB:Database Password={passkey};";
+        connection = new OleDbConnection(connectionString);
+        connection.Open();
+        Console.WriteLine("Connected to database." + connection.State);
+    }
+    
+
+    public void InsertEvent(Event eventToInsert)
+    {
+        const string query = "INSERT INTO Incident (IncidentID, incident_type, description, address, latitude, longitude, status, start_time, resolved_time) " +
+                             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        using (OleDbCommand command = new OleDbCommand(query, connection))
+        {
+            command.Parameters.AddWithValue("@IncidentID", eventToInsert.eventID);
+            command.Parameters.AddWithValue("@incident_type", eventToInsert.type);
+            command.Parameters.AddWithValue("@description", eventToInsert.description);
+            command.Parameters.AddWithValue("@address", eventToInsert.location.address);
+            command.Parameters.AddWithValue("@latitude", eventToInsert.location.latitude);
+            command.Parameters.AddWithValue("@longitude", eventToInsert.location.longitude);
+            command.Parameters.AddWithValue("@status", eventToInsert.status);
+            command.Parameters.AddWithValue("@start_time", eventToInsert.startTimestamp);
+            command.Parameters.AddWithValue("@resolved_time", eventToInsert.resolvedTimestamp);
+            
+            Console.Write(command.ExecuteNonQuery());
+            Console.WriteLine("Inserted event into database.");
+            connection.Close();
+        }
+    }
+}
--- Server/Server.cs
+++ Server/Server.cs
@@ -5,19 +5,23 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
 
 namespace EMS_NEA;
 
 class Server
 {
+    private static DatabaseManager database;
     private static readonly HttpListener listener = new HttpListener();
     private static Event receivedEvent;
 
     public static async Task Main(string[] args)
     {
         Event _event = await ListenForEvents();
-        Console.WriteLine();
-        Console.WriteLine(SerializeEvent(receivedEvent));
+        database = new DatabaseManager();
+        Console.WriteLine(receivedEvent.Serialize());
+        database.InsertEvent(receivedEvent);
     }
     
 
@@ -38,7 +42,7 @@ class Server
                 {
                     string json = await reader.ReadToEndAsync();
                     Console.WriteLine(json);
-                    receivedEvent = DeserializeEvent(json);
+                    receivedEvent = Event.Deserialize(json);
                 }
             }
             
@@ -50,30 +54,17 @@ class Server
             return receivedEvent;
         }
     }
-    
-    private static Event DeserializeEvent(string jsonEvent)
-    {
-        Event serializedEvent = JsonSerializer.Deserialize<Event>(jsonEvent);
-
-        return serializedEvent;
-    }
-
-    private static string SerializeEvent(Event eventToSerialize)
-    {
-        string json = JsonSerializer.Serialize(eventToSerialize);
-        return json;
-    }
 }
 
 class Event
 {
-    private int eventID;
-    private string type;
-    private string description;
-    private Location location;
-    private string startTimestamp;
-    private string resolvedTimestamp;
-    private string status;
+    public int eventID { get; set; }
+    public string type { get; set; }
+    public string description { get; set; }
+    public Location location { get; set; }
+    public string startTimestamp { get; set; }
+    public string resolvedTimestamp { get; set; }
+    public string status { get; set; }
 
     // Factory method to create Event by deserializing JSON
     public static Event Deserialize(string json)
@@ -89,7 +80,7 @@ class Event
 
 class Location
 {
-    private string address;
-    private string latitude;
-    private string longitude;
+    public string address { get; set; }
+    public double latitude { get; set; }
+    public double longitude { get; set; }
 }
